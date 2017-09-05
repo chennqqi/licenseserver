f7418779f3f8a39ff1f14aeda123592d69fe4a90
CHENQ <chennqqi@qq.com>
2017-09-05 21:24:59 +0800

    build ok

--- server/install.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-set -e -x
-docker stop $(docker ps -qa)
-docker rm $(docker ps -qa)
-docker rmi licenseserver:latest
-
-docker load < licenseserver.tar
-
-docker run -d --name licenseserver -v `pwd`/log/:/log/ -v `pwd`/license.db:/license.db -p 8080:8080 licenseserver
